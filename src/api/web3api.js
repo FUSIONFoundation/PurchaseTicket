bf1a731dd11e276ad03b13961a422ad8658c2d86
Bret <Bret@Fusion.org>
2018-11-27 17:56:33 -0500

    web set connection logic

--- src/api/web3api.js
+++ src/api/web3api.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-import Web3 from "web3";
+import _web3 from "web3";
 import EventEmitter from "events";
 
 // var password = "123456"
-// web3.setProvider(new web3.providers.HttpProvider("http://localhost:5488"));
+// _web3.setProvider(new _web3.providers.HttpProvider("http://localhost:5488"));
 
-let provider;
-let _web3;
 
 export default class web3Api extends Component {
   /**
@@ -15,41 +13,58 @@ export default class web3Api extends Component {
   constructor(props) {
     super(props);
     this.eventEmitter = new EventEmitter();
+    this._web3 = null
+    this.provider = null
   }
 
   static get web3() {
-    if (!_web3) {
-      _web3 = new Web3();
+    if (!this._web3) {
+      this._web3 = new _web3();
     }
-    return _web3;
+    return this._web3;
   }
 
   setNodeAddress(newNodeAddress) {
-    console.log("Attempting to reconnect...");
+    let httpOnly
+
+    console.log("Attempting connect");
     if (newNodeAddress.indexOf("ws") === 0) {
-      provider = new Web3.providers.WebsocketProvider(newNodeAddress);
+      this.provider = new _web3.providers.WebsocketProvider(newNodeAddress);
+      httpOnly = false
+      this.provider.on("connect", function() {
+        console.log("blockchain connected");
+        this.emit( 'connectstatus', ["connected"] , null )
+      });
     } else {
-      provider = new Web3.providers.HttpProvider(newNodeAddress);
+      this.provider = new _web3.providers.HttpProvider(newNodeAddress);
+      httpOnly = true
     }
-    provider.on("connect", function() {
-      console.log("blockchain Reconnected");
-    });
 
-    if (!_web3) {
-      _web3 = new Web3(provider);
+    if (!this._web3) {
+      this._web3 = new _web3(this.provider);
     } else {
-      _web3.setProvider(provider);
+      this._web3.setProvider(this.provider);
     }
-    provider.on("error", e => {
+
+    if  ( httpOnly ) {
+
+      return;
+    }
+    
+    this.provider.on("error", e => {
       console.error("Blockchain WS Error", e);
+      this.emit( 'connectstatus', ["error"] , e )
     });
 
-    provider.on("end", e => {
-      _web3 = null;
+    this.provider.on("end", e => {
+      this._web3 = null;
       console.log(
         "Blockchain disconnected will try to reconnect in 15 seconds"
       );
-      setTimeout(() => {}, 15 * 1000);
+      setTimeout(() => { 
+        this.emit( 'connectstatus', ["disconnected"] , null )
+        this.setNodeAddress(newNodeAddress) 
+      }, 1);
     });
   }
 
@@ -94,83 +109,83 @@ export default class web3Api extends Component {
   }
 }
 
-// web3._extend({
+// _web3._extend({
 // 	property: 'fsn',
 // 	methods: [
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'getBalance',
 // 			call: 'fsn_getBalance',
 // 			params: 3,
 // 			inputFormatter: [
 // 				null,
-// 				web3._extend.formatters.inputAddressFormatter,
-// 				web3._extend.formatters.inputDefaultBlockNumberFormatter
+// 				_web3._extend.formatters.inputAddressFormatter,
+// 				_web3._extend.formatters.inputDefaultBlockNumberFormatter
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'getAllBalances',
 // 			call: 'fsn_getAllBalances',
 // 			params: 2,
 // 			inputFormatter: [
-// 				web3._extend.formatters.inputAddressFormatter,
-// 				web3._extend.formatters.inputDefaultBlockNumberFormatter
+// 				_web3._extend.formatters.inputAddressFormatter,
+// 				_web3._extend.formatters.inputDefaultBlockNumberFormatter
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'getTimeLockBalance',
 // 			call: 'fsn_getTimeLockBalance',
 // 			params: 3,
 // 			inputFormatter: [
 // 				null,
-// 				web3._extend.formatters.inputAddressFormatter,
-// 				web3._extend.formatters.inputDefaultBlockNumberFormatter
+// 				_web3._extend.formatters.inputAddressFormatter,
+// 				_web3._extend.formatters.inputDefaultBlockNumberFormatter
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'getAllTimeLockBalances',
 // 			call: 'fsn_getAllTimeLockBalances',
 // 			params: 2,
 // 			inputFormatter: [
-// 				web3._extend.formatters.inputAddressFormatter,
-// 				web3._extend.formatters.inputDefaultBlockNumberFormatter
+// 				_web3._extend.formatters.inputAddressFormatter,
+// 				_web3._extend.formatters.inputDefaultBlockNumberFormatter
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'getNotation',
 // 			call: 'fsn_getNotation',
 // 			params: 2,
 // 			inputFormatter: [
-// 				web3._extend.formatters.inputAddressFormatter,
-// 				web3._extend.formatters.inputDefaultBlockNumberFormatter
+// 				_web3._extend.formatters.inputAddressFormatter,
+// 				_web3._extend.formatters.inputDefaultBlockNumberFormatter
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'getAddressByNotation',
 // 			call: 'fsn_getAddressByNotation',
 // 			params: 2,
 // 			inputFormatter: [
 // 				null,
-// 				web3._extend.formatters.inputDefaultBlockNumberFormatter
+// 				_web3._extend.formatters.inputDefaultBlockNumberFormatter
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'allNotation',
 // 			call: 'fsn_allNotation',
 // 			params: 1,
 // 			inputFormatter: [
-// 				web3._extend.formatters.inputDefaultBlockNumberFormatter
+// 				_web3._extend.formatters.inputDefaultBlockNumberFormatter
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'genNotation',
 // 			call: 'fsn_genNotation',
 // 			params: 2,
 // 			inputFormatter: [
-// 				web3._extend.formatters.inputTransactionFormatter,
+// 				_web3._extend.formatters.inputTransactionFormatter,
 // 				null
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'genAsset',
 // 			call: 'fsn_genAsset',
 // 			params: 2,
@@ -186,91 +201,91 @@ export default class web3Api extends Component {
 // 						throw new Error('invalid decimals');
 // 					}
 // 					if(options.total !== undefined){
-// 						options.total = web3.fromDecimal(options.total)
+// 						options.total = _web3.fromDecimal(options.total)
 // 					}
-// 					return web3._extend.formatters.inputTransactionFormatter(options)
+// 					return _web3._extend.formatters.inputTransactionFormatter(options)
 // 				},
 // 				null
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'allAssets',
 // 			call: 'fsn_allAssets',
 // 			params: 1,
 // 			inputFormatter: [
-// 				web3._extend.formatters.inputDefaultBlockNumberFormatter
+// 				_web3._extend.formatters.inputDefaultBlockNumberFormatter
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'sendAsset',
 // 			call: 'fsn_sendAsset',
 // 			params: 2,
 // 			inputFormatter: [
-// 				web3._extend.formatters.inputTransactionFormatter,
+// 				_web3._extend.formatters.inputTransactionFormatter,
 // 				null
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'assetToTimeLock',
 // 			call: 'fsn_assetToTimeLock',
 // 			params: 2,
 // 			inputFormatter: [
 // 				function(options){
-// 					return web3._extend.formatters.inputTransactionFormatter(options)
+// 					return _web3._extend.formatters.inputTransactionFormatter(options)
 // 				},
 // 				null
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'timeLockToTimeLock',
 // 			call: 'fsn_timeLockToTimeLock',
 // 			params: 2,
 // 			inputFormatter: [
 // 				function(options){
-// 					return web3._extend.formatters.inputTransactionFormatter(options)
+// 					return _web3._extend.formatters.inputTransactionFormatter(options)
 // 				},
 // 				null
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'timeLockToAsset',
 // 			call: 'fsn_timeLockToAsset',
 // 			params: 2,
 // 			inputFormatter: [
 // 				function(options){
-// 					return web3._extend.formatters.inputTransactionFormatter(options)
+// 					return _web3._extend.formatters.inputTransactionFormatter(options)
 // 				},
 // 				null
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'allTickets',
 // 			call: 'fsn_allTickets',
 // 			params: 1,
 // 			inputFormatter: [
-// 				web3._extend.formatters.inputDefaultBlockNumberFormatter
+// 				_web3._extend.formatters.inputDefaultBlockNumberFormatter
 // 			]
 // 		}),
-// 		new web3._extend.Method({
+// 		new _web3._extend.Method({
 // 			name: 'buyTicket',
 // 			call: 'fsn_buyTicket',
 // 			params: 2,
 // 			inputFormatter: [
-// 				web3._extend.formatters.inputTransactionFormatter,
+// 				_web3._extend.formatters.inputTransactionFormatter,
 // 				null
 // 			]
 // 		}),
 // 	],
 // 	properties:[
-// 		new web3._extend.Property({
+// 		new _web3._extend.Property({
 // 			name: 'coinbase',
 // 			getter: 'eth_coinbase'
 // 		}),
 // 	]
 // });
 
-// web3.eth.filter('latest').watch(function(err,log) {
-//     web3.fsn.buyTicket({from:web3.fsn.coinbase},password)
+// _web3.eth.filter('latest').watch(function(err,log) {
+//     _web3.fsn.buyTicket({from:_web3.fsn.coinbase},password)
 // });
 
 //fsn.assetToTimeLock({from:fsn.coinbase,to:fsn.coinbase,asset:"0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",start:"0x0",end:"0x1",value:"0x1111111"},"123456")
